f00224a3b3614fa373a1975f1e3b70ea877ac89b
RasoloarisonTina1 <rasoloarison@yahoo.fr>
2015-03-26 16:38:43 +0100

    Revert "added source files"
    This reverts commit 01080f4884d38642a87a4da9dcb11f91db8cc644.

--- src/nodes/NodeSpecialist.java
+++
@@ -1,9 +0,0 @@
-package nodes;
-
-import java.util.Iterator;
-
-public interface NodeSpecialist <T extends Node> {
-	public int getHeuristicDist(T start, T goal);
-	public Iterator<T> getAdjacents(T node);
-	public int getDistBetween(T node, T neighbour);
-}
